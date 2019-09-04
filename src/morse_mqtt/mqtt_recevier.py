67fae0e36637002abaa3258da642d8542973a3e1
Kostiantyn Fursenko <kostiantyn.fursenko@volvocars.com>
2019-09-04 14:08:52 +0200

    Add message queue

--- src/morse_mqtt/mqtt_recevier.py
+++ src/morse_mqtt/mqtt_recevier.py
@@ -1,9 +1,12 @@
 import paho.mqtt.client as mqtt
+from time import time
 
 class MqttReceiver:
-    def __init__(self, host, topic):
+    def __init__(self, host, topic, queue):
         self.host = host
         self.topic = topic
+        # class variable so on_message could be used
+        MqttReceiver.queue = queue
 
         self.client = mqtt.Client()
         self.client.on_connect = self._on_connect
@@ -11,12 +14,18 @@ class MqttReceiver:
         self.client.on_subscribe = self._on_subscribe
 
     @staticmethod
-    def _on_connect(client, _userdata, _flags, rc):
+    def get_queue():
+        if MqttReceiver.queue is None:
+            raise "No queue was provided"
+        return MqttReceiver.queue
+
+    @staticmethod
+    def _on_connect(_client, _userdata, _flags, rc):
         print('Connected with result code ' + str(rc))
 
     @staticmethod
     def _on_message(_client, _userdata, msg):
-        print('[' + msg.topic + ']: ' + str(msg.payload))
+        MqttReceiver.get_queue().put({time(): msg.payload})
 
     @staticmethod
     def _on_subscribe(_client, _userdata, _mid, _granted_qos):
