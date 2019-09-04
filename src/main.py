465d6c8057e7d654044bf2202ad82c35839d3de8
Oliver Lee <oliverzlee@gmail.com>
2019-09-04 11:39:17 +0200

    Encode and then decode text from stdin

--- src/main.py
+++ src/main.py
@@ -1,6 +1,36 @@
-def main():
-    pass
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from morse_mqtt.decode import Pulse, decode_pulses
+import morse_talk as mtalk
+
+
+# This will be replaced by the MQTT receiver
+def encode_pulses(line):
+    # Convert line to morse code binary encoding
+    bin_enc = mtalk.encode(line.strip(), encoding_type='binary')
+
+    # Convert binary encoding to a pulse sequence. We can just use something dumb for the timestamps for now.
+    timestamps = [0]
+    prev_value = bin_enc[0]
+    assert (prev_value[0] == '1')
+
+    for i, value in enumerate(bin_enc[1:], 1):
+        if value != prev_value:
+            prev_value = value
+            timestamps.append(i)
+
+    # Add the timestamp of the last falling edge
+    timestamps.append(i + 1)
+
+    # Group in pairs and create a pulse sequence
+    timestamps = [iter(timestamps)] * 2
+    return [Pulse(x, y) for (x, y) in zip(*timestamps)]
 
 
 if __name__ == "__main__":
-    main()
+    import fileinput
+
+    for line in fileinput.input():
+        pulses = encode_pulses(line)
+
+        print(decode_pulses(pulses))
